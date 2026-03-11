3be96d908e368579c26d474d9b57ab23cf424813
Shadow prime <gning8044@gmail.com>
2026-03-11 14:14:30 +0000

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -7,13 +7,13 @@ export async function uptime(client, message) {
     const minutes = Math.floor((uptime % 3600) / 60)
     const seconds = Math.floor(uptime % 60)
     
-    const text = `┌─🤖 SHADOW-TECH ─┐
+    const text = `┌─🤖 𝐀𝐊𝐀𝐓𝐒𝐔𝐊𝐈_𝐌𝐃 ─┐
 │
 │ ⏱️ Uptime: ${days}d ${hours}h ${minutes}m
 │ 💾 RAM: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(1)}MB
 │
-│ "SHADOW-TECH"
-│     - DC243 -
+│ "𝐀𝐊𝐀𝐓𝐒𝐔𝐊𝐈_𝐌𝐃"
+│     - 𝐀𝐊𝐀𝐓𝐒𝐔𝐊𝐈_𝐌𝐃 -
 └────────────────────┘`
     
     await client.sendMessage(remoteJid, { text: text })
