c926bf46af0d6d71c23709b5b287ea8641ff1e36
Shadow prime <gning8044@gmail.com>
2026-04-02 19:50:18 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,13 +4,13 @@ export async function pingTest(client, message) {
     const remoteJid = message.key.remoteJid
     const start = Date.now()
 
-    await client.sendMessage(remoteJid, { text: "🥷 𓊈𝐊𝐀𝐊𝐀𝐒𝐇𝐈-𝐌𝐃𓊉☠️..." }, { quoted: message })
+    await client.sendMessage(remoteJid, { text: "🥷 𝙕𝙀𝙏𝙎𝙐-𝙉𝙊𝙄𝙍-𝐌𝐃𓊉☠️..." }, { quoted: message })
 
     const latency = Date.now() - start
 
     await client.sendMessage(remoteJid, {
         text: stylizedChar(
-            `☠️ 𓊈𝐊𝐀𝐊𝐀𝐒𝐇𝐈-𝐌𝐃𓊉☠️ en ligne\n\n` +
+            `☠️ 𓊈𝙕𝙀𝙏𝙎𝙐-𝙉𝙊𝙄𝙍-𝐌𝐃𓊉☠️ en ligne\n\n` +
             `Latency: ${latency} ms\n\n` +
             `☠️𓊈*Bot Crée Par Dev Shadow Tech*𓊉☠️ `
         )
