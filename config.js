5fa2855177c62f47b233d074fd8cac8dd2942b55
Shadow prime <gning8044@gmail.com>
2026-05-04 19:24:28 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  Number: "‪221769325203‬" // remplacez par votre numéro WhatsApp sans mettre le signe +
+  Number: "‪221782120693‬" // remplacez par votre numéro WhatsApp sans mettre le signe +
 };
